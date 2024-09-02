7b3ac7363d7c8759fedccc8b0ce629b5b5ef1684
Hans Yu <hansyu@tesla.com>
2024-09-02 15:12:43 +0200

    Utility functions.

--- factorio_resource_planner/model/__init__.py
+++ factorio_resource_planner/model/__init__.py
@@ -1,2 +1,12 @@
+import typing
+
+import factorio_resource_planner.model.util
+
 from factorio_resource_planner.model.part import Part
 from factorio_resource_planner.model.recipe import Ingredient, Recipe
+
+def load_recipes() -> typing.List[Recipe]:
+    return factorio_resource_planner.model.util.load_models(Recipe, "recipes")
+
+def load_parts() -> typing.List[Part]:
+    return factorio_resource_planner.model.util.load_models(Part, "parts")
